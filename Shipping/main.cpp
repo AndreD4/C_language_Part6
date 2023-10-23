ede9bc2f5ad1fa2ec307463b03604318050a6dde
AndreD4 <andremai629@gmail.com>
2023-10-23 14:12:22 +0900

    base set up example for shipping

--- Shipping/main.cpp
+++ Shipping/main.cpp
@@ -4,6 +4,30 @@ using namespace std;
 
 int main() 
 {
-    cout << "Default Template" << endl;
+    
+    int length{}, width{}, height{};
+    double base_cost{2.50};
+    
+    const int tier1_threshold {100}; // volume
+    const int tier2_threshold {500}; // volume
+    
+    int max_dimension_length {10};
+    
+    double tier1_surcharge {0.10}; // 10% extra
+    double tier2_surcharge {0.25}; // 25% extra
+    
+    // All dimension must be 10 inches or less
+    
+    int packages_volume{};
+    
+    cout << "Welcome to the package cost calculator" << endl;
+    cout << "Enter length, width adn height of the package separated by spaces : ";
+    cin >> length >> width >> height;
+    
+    
+    
+    
+    
+    cout << endl;
     return 0;    
 }
