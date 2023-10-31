86c9f30db1e73f7fb275e9ddec7fd0cdbf47e56b
AndreD4 <andremai629@gmail.com>
2023-11-01 06:58:46 +0900

    Example

--- SwitchEnum/main.cpp
+++ SwitchEnum/main.cpp
@@ -4,6 +4,24 @@ using namespace std;
 
 int main() 
 {
+    enum Direction {
+        left, right, up, down
+    };
+    
+    Direction heading {left};
+    
+    switch (heading){
+    case left:
+        cout << "Going left" << endl;
+        break;
+    case right:
+        cout << "Going right" << endl;
+        break;
+    default:
+        cout << "Ok" << endl;
+    }
+    
+    
     cout << endl;
     return 0;    
 }
