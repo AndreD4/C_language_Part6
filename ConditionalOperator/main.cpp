8fe878015cdddb2ec9b4bf757c41f9713dce9cd8
AndreD4 <andremai629@gmail.com>
2023-11-02 15:28:17 +0900

    first example using if statement

--- ConditionalOperator/main.cpp
+++ ConditionalOperator/main.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main() 
 {
+    int num {};
+    
+    cout << "Enter an integer: ";
+    cin >> num;
+    
+    if (num % 2 == 0)
+        cout << num << " is even" << endl;
+    else
+        cout << num << " is odd" << endl;
     
     
     
