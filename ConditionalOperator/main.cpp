f85597f3ce737b67e0193cdb0611e33447d9cea4
AndreD4 <andremai629@gmail.com>
2023-11-02 15:38:04 +0900

    Simple example of using conditional operator

--- ConditionalOperator/main.cpp
+++ ConditionalOperator/main.cpp
@@ -4,21 +4,31 @@ using namespace std;
 
 int main() 
 {
-    int num {};
+//    int num {};
+//    
+//    cout << "Enter an integer: ";
+//    cin >> num;
+//    
+//    if (num % 2 == 0)
+//        cout << num << " is even" << endl;
+//    else
+//        cout << num << " is odd" << endl;
+//    
+//    
+//    cout << num << " is " << ( (num %2 ==0) ? "even" : "odd" ) << endl;
+        
     
-    cout << "Enter an integer: ";
-    cin >> num;
+    int num1{}, num2{};
     
-    if (num % 2 == 0)
-        cout << num << " is even" << endl;
-    else
-        cout << num << " is odd" << endl;
+    cout << "Enter two integers separated by space: ";
+    cin >> num1 >> num2;
     
-    
-    cout << num << " is " << ( (num %2 ==0) ? "even" : "odd" ) << endl;
-    
-    
-    
+    if (num1 != num2){
+        cout << "Largest: " << ( (num1 > num2) ? num1 : num2 ) << endl;
+        cout << "Smallest: " << ( (num1 < num2) ? num1 : num2 ) << endl;
+    }else{
+        cout << "numbers are the same" << endl;
+    }
     
     cout << endl;
     return 0;    
