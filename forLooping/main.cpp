f0954717a12eadd10e109a8c67ab590a04726da6
AndreD4 <andremai629@gmail.com>
2023-11-08 08:39:35 +0900

    comma for loop

--- forLooping/main.cpp
+++ forLooping/main.cpp
@@ -17,12 +17,13 @@ int main()
 //    cout << "Blast off" << endl;
 
 
-    for (int i{10} ; i <=100 ; i+=10 ){
-        if(i % 15 == 0)
-            cout << i << endl;
-    
-    }
-    
+//    for (int i{10} ; i <=100 ; i+=10 ){
+//        if(i % 15 == 0)
+//            cout << i << endl;
+//    }
+
+    for (int i{1}, j{5}; i <= 5; ++i, ++j)
+        cout << i << " + " << j << " = " << (i+j) << endl;
     
     
     cout << endl;
