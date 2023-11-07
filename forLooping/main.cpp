30dc346a38978b39066dd6b03664be663b7815c5
AndreD4 <andremai629@gmail.com>
2023-11-08 08:27:03 +0900

    for loop - counting up

--- forLooping/main.cpp
+++ forLooping/main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main() 
 {
-    cout << "Default Template" << endl;
+    
+//    for (int i{1} ; i <=10 ; ++i)
+//        cout << i << endl;
+
+
+    for (int i{1} ; i <=10 ; i+=2)
+        cout << i << endl;
+    
+    
+    
+    
+    
+    
+    
+    cout << endl;
     return 0;    
 }
